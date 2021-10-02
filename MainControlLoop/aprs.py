8174e49cde9a2c8d4817db234aa6ecd8a55cb1bd
nik-diary <skdc13127@gmail.com>
2021-10-02 12:40:28 -0400

    aprs.functional

--- MainControlLoop/aprs.py
+++ MainControlLoop/aprs.py
@@ -35,9 +35,9 @@ class APRS:
             except:
                 return False
         self.serial.flush()
-        self.serial.write("\x1b\x1b\x1b\n".encode("utf-8"))
-        self.serial.write("\x1b\x1b\x1b\n".encode("utf-8"))
-        self.serial.write("\x1b\x1b\x1b".encode("utf-8"))
+        self.serial.write(b"\x1b\x1b\x1b\n")
+        self.serial.write(b"\x1b\x1b\x1b\n")
+        self.serial.write(b"\x1b\x1b\x1b")
         """time.sleep(.3)
         self.write("MYCALL")
         try:
