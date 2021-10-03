2b2b15b3bc31aa3fb0ae41e9c465b313c8209067
nik-diary <skdc13127@gmail.com>
2021-10-02 21:31:44 -0400

    aprs.functional

--- MainControlLoop/aprs.py
+++ MainControlLoop/aprs.py
@@ -36,12 +36,16 @@ class APRS:
                 return False
         self.serial.flush()
         self.serial.write("\x1b\x1b\x1b\n".encode("utf-8"))
+        print(self.read())
         self.serial.write("\x1b\x1b\x1b\n".encode("utf-8"))
+        print(self.read())
         self.serial.write("\x1b\x1b\x1b".encode("utf-8"))
+        print(self.read())
+        time.sleep(.5)
+        print(self.read())
         try:
             # For now, just reads first byte, and if byte exists and isn't empty.
             byte = self.serial.read(size=1)
-            print("byte read successfully")
             # Can be updated to match what the message actually is and match it to an expected value
             # once we get a good idea of what we expect from MYCALL
         except:
