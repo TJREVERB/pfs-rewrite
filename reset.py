0dee411e4847b0714fc6559b652858dfabb7cf19
tjreverb <tjreverb@gmail.com>
2021-10-06 02:30:11 -0400

    DO NOT RUN RESET.PY. call exec(open(reset.py).read()) in the python shell to avoid corrupting the file

--- reset.py
+++ reset.py
@@ -1,4 +1,10 @@
+# DO NOT RUN!!!
+# Use exec(open("reset.py").read()) in python shell to avoid corrupting file
 from MainControlLoop.lib.StateFieldRegistry.registry import StateFieldRegistry
+from MainControlLoop.eps import EPS
 
 
-StateFieldRegistry().reset()
+sfr = StateFieldRegistry()
+eps = EPS(sfr)
+sfr.reset()
+eps.commands["Bus Reset"](["Battery", "5V", "3.3V", "12V"])
