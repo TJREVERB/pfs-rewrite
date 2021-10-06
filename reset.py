dfb29fbdc337fa44df6e6d31dfa3531179df0e32
Nikhil Kalidasu <skdc13129@gmail.com>
2021-10-06 14:04:17 -0400

    reset

--- reset.py
+++ reset.py
@@ -1,12 +1,4 @@
 from MainControlLoop.lib.StateFieldRegistry.registry import StateFieldRegistry
-from MainControlLoop.eps import EPS
 
 
-class Reset:
-    def __init__(self):
-        self.sfr = StateFieldRegistry()
-        self.eps = EPS(self.sfr)
-    def reset(self):
-        self.sfr.reset()
-        self.eps.commands["Bus Reset"](["Battery", "5V", "3.3V", "12V"])
-        exit(0)
+StateFieldRegistry().reset()
