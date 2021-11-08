b2e56a426ef3acaa6dffc6cf29602d7751c24959
Alan-JH <alanjhsu08@gmail.com>
2021-11-08 08:30:25 -0500

    add struct stuff back in because Im too lazy to reverse engineer adafruit's implementation

--- MainControlLoop/bno055.py
+++ MainControlLoop/bno055.py
@@ -23,6 +23,97 @@ try:
 except ImportError:
     import ustruct as struct
 
+class Struct:
+    """
+    Arbitrary structure register that is readable and writeable.
+    Values are tuples that map to the values in the defined struct.  See struct
+    module documentation for struct format string and its possible value types.
+    :param int register_address: The register address to read the bit from
+    :param type struct_format: The struct format string for this register.
+    """
+
+    def __init__(self, register_address, struct_format):
+        self.format = struct_format
+        self.buffer = bytearray(1 + struct.calcsize(self.format))
+        self.buffer[0] = register_address
+
+    def __get__(self, obj, objtype=None):
+        with obj.i2c_device as i2c:
+            i2c.write_then_readinto(self.buffer, self.buffer, out_end=1, in_start=1)
+        return struct.unpack_from(self.format, memoryview(self.buffer)[1:])
+
+    def __set__(self, obj, value):
+        struct.pack_into(self.format, self.buffer, 1, *value)
+        with obj.i2c_device as i2c:
+            i2c.write(self.buffer)
+
+
+class UnaryStruct:
+    """
+    Arbitrary single value structure register that is readable and writeable.
+    Values map to the first value in the defined struct.  See struct
+    module documentation for struct format string and its possible value types.
+    :param int register_address: The register address to read the bit from
+    :param type struct_format: The struct format string for this register.
+    """
+
+    def __init__(self, register_address, struct_format):
+        self.format = struct_format
+        self.address = register_address
+
+    def __get__(self, obj, objtype=None):
+        buf = bytearray(1 + struct.calcsize(self.format))
+        buf[0] = self.address
+        with obj.i2c_device as i2c:
+            i2c.write_then_readinto(buf, buf, out_end=1, in_start=1)
+        return struct.unpack_from(self.format, buf, 1)[0]
+
+    def __set__(self, obj, value):
+        buf = bytearray(1 + struct.calcsize(self.format))
+        buf[0] = self.address
+        struct.pack_into(self.format, buf, 1, value)
+        with obj.i2c_device as i2c:
+            i2c.write(buf)
+
+
+class _ScaledReadOnlyStruct(Struct):  # pylint: disable=too-few-public-methods
+    def __init__(self, register_address, struct_format, scale):
+        super().__init__(register_address, struct_format)
+        self.scale = scale
+
+    def __get__(self, obj, objtype=None):
+        result = super().__get__(obj, objtype)
+        return tuple(self.scale * v for v in result)
+
+    def __set__(self, obj, value):
+        raise NotImplementedError()
+
+class _ReadOnlyUnaryStruct(UnaryStruct):  # pylint: disable=too-few-public-methods
+    def __set__(self, obj, value):
+        raise NotImplementedError()
+
+
+class _ModeStruct(Struct):  # pylint: disable=too-few-public-methods
+    def __init__(self, register_address, struct_format, mode):
+        super().__init__(register_address, struct_format)
+        self.mode = mode
+
+    def __get__(self, obj, objtype=None):
+        last_mode = obj.mode
+        obj.mode = self.mode
+        result = super().__get__(obj, objtype)
+        obj.mode = last_mode
+        # single value comes back as a one-element tuple
+        return result[0] if isinstance(result, tuple) and len(result) == 1 else result
+
+    def __set__(self, obj, value):
+        last_mode = obj.mode
+        obj.mode = self.mode
+        # underlying __set__() expects a tuple
+        set_val = value if isinstance(value, tuple) else (value,)
+        super().__set__(obj, set_val)
+        obj.mode = last_mode
+
 class IMU:
     """
     Base class for the BNO055 9DOF IMU sensor.
