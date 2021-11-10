63f5cc9d695f6869b0a851b46885edb189d1b428
The-A44 <arthur.prudius@gmail.com>
2021-11-10 15:45:19 -0500

    wrote outreach mode

--- MainControlLoop/Mode/outreach.py
+++ MainControlLoop/Mode/outreach.py
@@ -1,5 +1,8 @@
 from MainControlLoop.Mode.mode import Mode
+from MainControlLoop.Mode.charging import Charging
+
 import gc
+import time
 
 
 """
@@ -25,14 +28,38 @@ class Outreach(Mode):  # TODO: IMPLEMENT
 
         self.limited_command_registry = {
             # Reads and transmits battery voltage
-            "BVT": lambda: self.aprs.write("TJ;" + str(self.eps.telemetry["VBCROUT"]())),
+            "BVT": lambda: self.sfr.devices["APRS"].write("TJ;" + str(self.eps.telemetry["VBCROUT"]())),
             # Transmit total power draw of connected components
-            "PWR": lambda: self.aprs.write("TJ;" + str(self.eps.total_power(3)[0])),
+            "PWR": lambda: self.sfr.devices["APRS"].write("TJ;" + str(self.eps.total_power(3)[0])),
             # Calculate and transmit Iridium signal strength variability
-            "SSV": lambda: self.aprs.write("TJ;SSV:" + str(self.sfr.signal_strength_variability())),
+            "SSV": lambda: self.sfr.devices["APRS"].write("TJ;SSV:" + str(self.sfr.signal_strength_variability())),
             # Transmit current solar panel production
-            "SOL": lambda: self.aprs.write("TJ;SOL:" + str(self.eps.solar_power())),
+            "SOL": lambda: self.sfr.devices["APRS"].write("TJ;SOL:" + str(self.eps.solar_power())),
         }
 
     def __str__(self):
         return "Outreach"
+
+    def start(self):
+        self.eps.commands["All On"]()
+
+    def check_conditions(self):
+        if self.sfr.eps.telemetry["VBCROUT"]() > self.LOWER_THRESHOLD:  # if voltage greater than lower thres
+            return True
+        else:
+            return False
+
+    def execute_cycle(self):
+        self.sfr.devices["APRS"].read()
+            
+        if(self.sfr.APRS_RECIEVED_COMMAND != ""): # Check if there are any commands from outreach
+            raw_command = self.sfr.APRS_RECIEVED_COMMAND
+            command = raw_command[raw_command.find("TJ;") + 3:raw_command.find("TJ;") + 6]
+            self.limited_command_registry[command]()
+
+    
+    def switch_modes(self):
+        return Charging
+
+    def terminate_mode(self):
+        self.instruct["Pin Off"]("APRS")
