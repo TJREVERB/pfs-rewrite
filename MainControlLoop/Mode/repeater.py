87582ca77d3e051c4f88c80d13744ff4f73014ed
Nicolas-M1 <nickmakovnik@gmail.com>
2021-11-12 15:52:51 -0500

    Added Repeater switch_modes

--- MainControlLoop/Mode/repeater.py
+++ MainControlLoop/Mode/repeater.py
@@ -1,10 +1,16 @@
 from MainControlLoop.Mode.mode import Mode
+from MainControlLoop.Mode.mode import Outreach
+from MainControlLoop.Mode.mode import Charging
 import gc
 
 
 class Repeater(Mode):  # TODO: IMPLEMENT
     def __init__(self, sfr):
         super().__init__(sfr)
+
+        self.conditions = {
+            "CHARGE_LOW": False
+        }
 
 
     def __str__(self):
@@ -15,11 +21,11 @@ class Repeater(Mode):  # TODO: IMPLEMENT
         self.instruct["Pin On"]("APRS")
 
     def check_conditions(self) -> bool:
-        current_voltage = self.sfr.eps.telemetry["VBCROUT"]()
-        if current_voltage > self.LOWER_THRESHOLD:  # if voltage is less than upper limit
-            return True
-        else:
+        self.conditions["CHARGE_LOW"] = self.sfr.eps.telemetry["VBCROUT"]() > self.LOWER_THRESHOLD
+        if self.conditions["CHARGE_LOW"]:  # if voltage is less than upper limit
             return False
+        else:
+            return True
 
     def execute_cycle(self) -> None:
 
@@ -27,7 +33,12 @@ class Repeater(Mode):  # TODO: IMPLEMENT
         self.sfr.dump()  # Log changes
 
     def switch_modes(self) -> None:
-        pass
+        super(Outreach, self).switch_modes()  # Run switch_modes of superclass
+
+        if self.conditions["CHARGE_LOW"]:  # if the battery is low, switch to charging mode
+            return Charging
+        
+        return Outreach #if this is called even though the conditions are met, this just returns itself
 
     def terminate_mode(self) -> None:
         # TODO: write to APRS to turn off digipeating
