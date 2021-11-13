ee0c2a2550e6b750bc81d7997aa614ccf8438c8b
Alan-JH <alanjhsu08@gmail.com>
2021-11-13 17:11:23 -0500

    Update softserial.py

--- MainControlLoop/softserial.py
+++ MainControlLoop/softserial.py
@@ -6,13 +6,13 @@ class SoftwareUART():
     """Bitbang uart driver using pigpio and interrupts"""
     
     def __init__(self, RX, TX, baud):
-        self.pi = pigpio.pi()
-        # test to see if the daemon is already running
-        if not self.pi.connected:
-            # if the daemon is not running, start it
+        try:
+            self.pi = pigpio.pi()
+        except:
             os.system("sudo pigpiod")
             time.sleep(1)
             self.pi = pigpio.pi()
+            
         self.RXPin = RX
         self.TXPin = TX
         self.baudrate = baud
