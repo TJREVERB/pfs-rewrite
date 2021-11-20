75703e764b999a81e5cd46b458909dfb6e5aea88
Alan-JH <alanjhsu08@gmail.com>
2021-11-20 16:19:11 -0500

    clean up the command execution and command registries

--- MainControlLoop/command_executor.py
+++ MainControlLoop/command_executor.py
@@ -6,8 +6,11 @@ class CommandExecutor:
         self.sfr = sfr
         self.TJ_PREFIX = "TJ;"
         self.OUTREACH_PREFIX = "OUT;"
-        self.aprs_primary_registry = {
-            "NOP": lambda: self.transmit("Hello"),  # Test method, transmits "Hello"
+
+        self.GARBLED = "GRB" #String for garbled message
+
+        self.primary_registry = { #primary command registry for BOTH Iridium and APRS
+            "NOP": lambda: self.transmit("0OK"),  # Test method, transmits "Hello"
             "BVT": self.BVT,  # Reads and transmits battery voltage
             "CHG": self.CHG,  # Enters charging mode
             "SCI": self.SCI,  # Enters science mode
@@ -23,21 +26,24 @@ class CommandExecutor:
             "SVF": None,  # TODO: Implement #Transmit full rssi data logs
             # Transmit current solar panel production
             "SOL": self.SOL,
-            "TBL": self.TBL,  # Transmits tumble value
+            "TBL": self.TBL,  # Transmits tumble value (Magnitude of gyroscope vector)
+            "TBF": self.TBF,  # Transmits 3 axis gyroscope and magnetometer readings (full tumble readouts)
             "MLK": self.MLK,
             "ORB": self.ORB,
-            "Arguments": { #this doesn't make sense, we'd have to call primary_registry["arguments"].keys() to check if a string is one of these commands
-                "U": self.U,  # Set upper threshold
-                "L": self.L,  # Set lower threshold
-                "D": self.D
-            }
+            "UVT": self.UVT, # Set upper threshold
+            "LVT": self.LVT, # Set lower threshold
+            "DLK": self.DLK,
         }
+
         # IMPLEMENT FULLY
-        self.aprs_secondary_registry = {
+        self.aprs_secondary_registry = { #Secondary command registry for APRS, in outreach mode
             # Reads and transmits battery voltage
             "BVT": lambda: self.sfr.devices["Iridium"].transmit(str(self.sfr.eps.telemetry["VBCROUT"]())),
-            "Arguments": { #this doesn't make sense
-            }
+        }
+        self.arg_registry = { # Set of commands that require arguments, for either registry, for error checking reasons only
+            "UVT",
+            "LVT",
+            "DLK"
         }
     
     def execute(self) -> None:
@@ -46,15 +52,22 @@ class CommandExecutor:
         """
         if self.sfr.devices["Iridium"] is not None:  # if iridium is on
             # IRIDIUM
-            for i in self.sfr.IRIDIUM_RECEIVED_COMMAND:  # Iterate through all received commands
-                if i in self.iridium_registry.keys:  # If command exists
-                    self.iridium_registry[i]()  # Execute command
-                elif i[0] in self.iridium_registry["Arguments"].keys:  # If command has arguments
-                    self.iridium_registry["Arguments"][i[0]](i[1], i[2])  # Execute command
-                else:
-                    self.error("Iridium", self.sfr.IRIDIUM_RECEIVED_COMMAND)  # Transmit error
-            self.sfr.IRIDIUM_RECEIVED_COMMAND = []  # Clear buffer
-        # APRS
+            while len(self.sfr.IRIDIUM_RECEIVED_COMMAND) > 0:  # Iterate through all received commands
+                cmd = self.sfr.IRIDIUM_RECEIVED_COMMAND.pop(0)
+                msn = cmd[1]
+                prcmd = cmd[0].split(":") 
+
+                if prcmd[0] in self.primary_registry.keys():  # If command exists
+                    try:
+                        if len(prcmd) == 2 and prcmd[0] in self.arg_registry: #If an argument is included and the command actually requires an argument, execute with arg
+                            self.primary_registry[prcmd[0]](prcmd[1])
+                        elif len(prcmd) == 1 and prcmd[0] not in self.arg_registry: #If an argument is not included, and the command does not require an argument, execute without arg
+                            self.primary_registry[prcmd[0]]() 
+                        else:
+                            self.error("Iridium", msn, "Incorrect number of arguments received")
+                    except Exception as e:
+                        self.error("Iridium", msn, "Exec error: " + str(e)) #returns exception
+        # APRS TODO: fix this absolute monstrosity of code
         if self.sfr.devices["APRS"] is not None:  # if APRS is on
             if self.sfr.APRS_RECEIVED_COMMAND != "":  # If message was received
                 raw_command = self.sfr.APRS_RECEIVED_COMMAND
@@ -63,8 +76,9 @@ class CommandExecutor:
                                             len(self.TJ_PREFIX):
                                             raw_command.find(self.TJ_PREFIX) +
                                             len(self.TJ_PREFIX) + 3]
-                    if command in self.aprs_primary_registry.keys:  # If command is real
-                        self.aprs_primary_registry[command]()  # Execute command
+                    
+                    if command in self.primary_registry.keys():  # If command is real
+                        self.primary_registry[command]()  # Execute command
                     elif i[0] in self.aprs_primary_registry["Arguments"].keys:  # If command has arguments
                         self.aprs_primary_registry["Arguments"][i[0]](i[1], i[2])  # Execute command
                     else:
@@ -82,16 +96,17 @@ class CommandExecutor:
                         self.error("APRS", command)  # Transmit error message
                 self.sfr.APRS_RECEIVED_COMMAND = ""  # Clear buffer
     
-    def error(self, radio, command):
+    def error(self, radio, command, description):
         """
         Transmit an error message over radio that received command
         :param radio: (str) radio which received erraneous command, "Iridium" or "APRS"
-        :param command: (str) command which failed
+        :param command: (str) command which failed in case of APRS, or MSN number of failed command in case of Iridium
+        :param description: (str) detailed description of failure
         """
         if radio == "Iridium":
-            self.sfr.devices["Iridium"].transmit("ERR:" + command)
+            self.sfr.devices["Iridium"].transmit("ERR" + command + description) #DO NOT ADD PUNCTUATION TO IRIDIUM MESSAGES, IT MESSES UP ENCODING
         elif radio == "APRS":
-            self.sfr.devices["APRS"].transmit("ERR:" + command)
+            self.sfr.devices["APRS"].transmit("ERR:" + command + ":" + description)
 
     def transmit(self, message: str):
         """
@@ -139,11 +154,11 @@ class CommandExecutor:
             self.sfr.MODE = self.sfr.modes_list["Outreach"]
             self.transmit("SWITCH OUTREACH")
 
-    def U(self, a, b):  #TODO: Implement
-        self.sfr.UPPER_THRESHOLD = int(a) + float(b) / 10
+    def UVT(self, v):  #TODO: Implement
+        self.sfr.UPPER_THRESHOLD = float(v)
 
-    def L(self, a, b):  #TODO: Implement
-        self.sfr.LOWER_THRESHOLD = int(a) + float(b) / 10
+    def LVT(self, v):  #TODO: Implement
+        self.sfr.LOWER_THRESHOLD = float(v)
 
     def RST(self):  #TODO: Implement, how to power cycle satelitte without touching CPU power
         self.sfr.mode_obj.instruct["All Off"](exceptions=[])
@@ -177,7 +192,12 @@ class CommandExecutor:
     
     def TBL(self):
         """
-        Transmit IMU tumble
+        Transmit magnitude IMU tumble
+        """
+    
+    def TBF(self):
+        """
+        Transmit full IMU tumble
         """
     
     def MLK(self):
@@ -185,7 +205,7 @@ class CommandExecutor:
         Enable Mode Lock
         """
     
-    def D(self, a, b):
+    def DLK(self, a, b):
         """
         Enable Device Lock
         """
