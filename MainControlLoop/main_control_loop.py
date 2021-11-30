b8c0c069f63b4a18e544f4ff1818e217a49fa491
Nikhil Kalidasu <skdc13129@gmail.com>
2021-11-30 14:26:54 -0500

    remove nested while loop in mcl

--- MainControlLoop/main_control_loop.py
+++ MainControlLoop/main_control_loop.py
@@ -15,26 +15,26 @@ class MainControlLoop:
     def run(self):  # Repeat main control loop forever
         print("MCL Start")
         current_time = time.time()
+        self.sfr.mode_obj = self.sfr.vars.MODE(self.sfr)
+        self.sfr.mode_obj.start()
         while True:  # Iterate forever
-            self.sfr.mode_obj = self.sfr.vars.MODE(self.sfr)
-            self.sfr.mode_obj.start()
-            # Iterate while we're supposed to be in this mode
-            while isinstance(self.sfr.mode_obj, self.sfr.vars.MODE):
 
-                # Update the conditions dictionary periodically for this mode:
-                if current_time + 1 <= time.time():  # if waited 1 second or more, update conditions dict in mode
-                    self.sfr.mode_obj.update_conditions()
-                    current_time = time.time()
+            if not isinstance(self.sfr.mode_obj, self.sfr.vars.MODE):
+                self.sfr.mode_obj.terminate_mode()  # terminates current old mode
+                self.sfr.mode_obj = self.sfr.vars.MODE(self.sfr)
+                self.sfr.mode_obj.start()
 
-                # If the conditions of this mode aren't met and we are allowed to switch:
-                if not self.sfr.mode_obj.check_conditions() and not self.sfr.vars.MODE_LOCK:
-                    # change the mode to be whatever our current mode wants to switch to
-                    self.sfr.vars.MODE = self.sfr.mode_obj.switch_mode()
-                    break
+            # Update the conditions dictionary periodically for this mode:
+            if current_time + 1 <= time.time():  # if waited 1 second or more, update conditions dict in mode
+                self.sfr.mode_obj.update_conditions()
+                current_time = time.time()
 
-                print("Cycle")
-                self.sfr.mode_obj.execute_cycle()  # Execute single cycle of mode
-                self.sfr.command_executor.execute()  # Execute commands
-                self.logger.execute_cycle()  # Update logs
+            # If the conditions of this mode aren't met and we are allowed to switch:
+            if not self.sfr.mode_obj.check_conditions() and not self.sfr.vars.MODE_LOCK:
+                # change the mode to be whatever our current mode wants to switch to
+                self.sfr.vars.MODE = self.sfr.mode_obj.switch_mode()
 
-            self.sfr.mode_obj.terminate_mode()  # terminates current old mode
+            print("Cycle")
+            self.sfr.mode_obj.execute_cycle()  # Execute single cycle of mode
+            self.sfr.command_executor.execute()  # Execute commands
+            self.logger.execute_cycle()  # Update logs
