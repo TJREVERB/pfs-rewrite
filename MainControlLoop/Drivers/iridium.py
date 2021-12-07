2e9f6129044481b0178090e949f1c97e0782d16d
Alan-JH <alanjhsu08@gmail.com>
2021-12-06 19:54:23 -0500

    implement processed geolocation

--- MainControlLoop/Drivers/iridium.py
+++ MainControlLoop/Drivers/iridium.py
@@ -510,15 +510,16 @@ class Iridium:
         Requests, reads, processes, and returns current geolocation
         :return: (tuple) lat, long, altitude (0,0,0 if unable to retrieve)
         """
-        self.SBD_INITIATE_EX() # Use SBDIX to update geolocation
         raw = self.process(self.GEO_C(), "MSGEO").split(",") # raw x, y, z, timestamp
-        if datetime.datetime.utcnow().timestamp() - (int(raw[3], 16) * 90 / 1000 + Iridium.EPOCH) > 60: # Checks if time passed since last geolocation update has been more than 60 seconds
-            return (0, 0, 0) # Return 0, 0, 0 if SBDIX fails
+        if self.processed_time() - (int(raw[3], 16) * 90 / 1000 + Iridium.EPOCH) > 60: # Checks if time passed since last geolocation update has been more than 60 seconds
+            result = [int(s) for s in self.process(self.SBD_INITIATE_EX(), "SBDIX").split(",")] # Use SBDIX to update geolocation
+            if result[0] not in [0, 1, 3, 4]:
+                return (0, 0, 0) # Return 0, 0, 0 if SBDIX fails
+            raw = self.process(self.GEO_C(), "MSGEO").split(",") # try again
         lon = math.atan(float(raw[1])/float(raw[0]))
         lat = math.atan(float(raw[2])/((float(raw[1])**2 + float(raw[0])**2)**0.5))
         alt = (float(raw[0])**2 + float(raw[1])**2 + float(raw[2])**2)**0.5
         return (lat, lon, alt)
-        #TODO: Implement
 
     def request(self, command: str, timeout=0.5) -> str:
         """
