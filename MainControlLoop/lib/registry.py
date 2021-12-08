38ea6676d9d262cfea4f912c5317b46f6a50c40f
The-A44 <arthur.prudius@gmail.com>
2021-12-08 18:47:20 -0500

    manual __str__ call

--- MainControlLoop/lib/registry.py
+++ MainControlLoop/lib/registry.py
@@ -104,7 +104,7 @@ class StateFieldRegistry:
             self.LAST_MODE_SWITCH = time.time()
         
         def encode(self):
-            print(self.MODE)
+            print(self.MODE.__str__())
             return [
                 int(self.ANTENNA_DEPLOYED),
                 self.BATTERY_CAPACITY_INT,
