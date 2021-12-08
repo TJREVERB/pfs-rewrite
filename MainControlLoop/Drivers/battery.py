5b3131fbfb408a42b141da297af9e7e572ed4a28
The-A44 <arthur.prudius@gmail.com>
2021-12-08 18:58:11 -0500

    did debug prints break it or did it just break on its own

--- MainControlLoop/Drivers/battery.py
+++ MainControlLoop/Drivers/battery.py
@@ -95,9 +95,6 @@ class Battery:
         :return: (float) telemetry value
         """
         raw = self.request(0x10, tle, 2)
-        print(raw[0])
-        print(raw[1])
-        print(multiplier)
         return (raw[0] << 8 | raw[1]) * multiplier
 
     def charging_power(self) -> float:
