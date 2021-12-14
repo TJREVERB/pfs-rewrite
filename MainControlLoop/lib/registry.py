0a2d0336b8671d6af608c5556e59249890e88ef8
nik875 <skdc13129@gmail.com>
2021-12-14 13:01:21 -0500

    bugfix

--- MainControlLoop/lib/registry.py
+++ MainControlLoop/lib/registry.py
@@ -229,7 +229,7 @@ class StateFieldRegistry:
         if len(df := pd.read_csv(self.pwr_log_path, header=0)) == 0:
             return [self.eps.bus_power()] + self.eps.raw_pdm_draw()[1]
         cols = ["buspower"] + [f"0x0{str(hex(i))[2:].upper()}" for i in range(1, 11)]
-        return df[cols].iloc[-1].tolist()
+        return df.loc[cols].iloc[-1].tolist()
 
     @wrap_errors(LogicalError)
     def recent_gen(self) -> list:
