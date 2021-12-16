a282076335120afa53d2b8410530360e21e6e9d6
Alan-JH <alanjhsu08@gmail.com>
2021-12-16 15:21:43 -0500

    correct transmission queue logic

--- MainControlLoop/Drivers/transmission_packet.py
+++ MainControlLoop/Drivers/transmission_packet.py
@@ -3,11 +3,12 @@ from MainControlLoop.lib.exceptions import wrap_errors, LogicalError
 
 class TransmissionPacket:
     @wrap_errors(LogicalError)
-    def __init__(self, command_string: str, args: list, msn: int, simulate=False):
+    def __init__(self, command_string: str, args: list, msn: int, simulate=False, outreach=False):
         self.command_string = command_string
         self.args = args
         self.msn = msn
         self.simulate = simulate
+        self.outreach = outreach
         self.timestamp = ()
         self.return_code = ""
         self.return_data = []
