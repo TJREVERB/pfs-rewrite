d23055f3b842f55ef09293f26e52b72d73d41a57
nik-diary <skdc13129+diary@gmail.com>
2021-12-15 20:13:34 -0500

    bugfix

--- MainControlLoop/Mode/mode.py
+++ MainControlLoop/Mode/mode.py
@@ -1,10 +1,7 @@
 import time
-from MainControlLoop.Drivers.aprs import APRS
-from MainControlLoop.Drivers.iridium import Iridium
-from MainControlLoop.Drivers.bno055 import IMU
-from MainControlLoop.Drivers.antenna_deployer.AntennaDeployer import AntennaDeployer
 from MainControlLoop.lib.exceptions import wrap_errors, LogicalError
-import datetime, os
+import datetime
+import os
 
 
 class Mode:
@@ -20,7 +17,7 @@ class Mode:
         # seconds)
         # Actual time between read/write will depend on signal availability
         self.SIGNAL_THRESHOLD = thresh  # Lower threshold to read or transmit
-        self.TIME_ERR_THRESHOLD = 120 # Two minutes acceptable time error between iridium network and rtc
+        self.TIME_ERR_THRESHOLD = 120  # Two minutes acceptable time error between iridium network and rtc
         self.sfr.instruct = {
             "Pin On": self.sfr.turn_on_component,
             "Pin Off": self.sfr.turn_off_component,
@@ -111,8 +108,8 @@ class Mode:
         :return: (bool) whether all transmit queue messages were sent
         """
         if self.sfr.vars.PRIMARY_RADIO == "APRS" or (self.sfr.vars.PRIMARY_RADIO == "Iridium" and
-            time.time() - self.last_iridium_poll_time > self.PRIMARY_IRIDIUM_WAIT_TIME and
-            self.sfr.devices["Iridium"].check_signal_passive() >= self.SIGNAL_THRESHOLD):
+                 time.time() - self.last_iridium_poll_time > self.PRIMARY_IRIDIUM_WAIT_TIME and
+                 self.sfr.devices["Iridium"].check_signal_passive() >= self.SIGNAL_THRESHOLD):
             print("Attempting to transmit queue")
             while len(self.sfr.vars.transmit_buffer) > 0:  # attempt to transmit transmit buffer
                 if not self.sfr.command_executor.transmit(p := self.sfr.vars.transmit_buffer.pop(0)):
@@ -132,9 +129,9 @@ class Mode:
             current_datetime = datetime.datetime.utcnow()
             iridium_datetime = self.sfr.devices["Iridium"].processed_time()
             if abs((current_datetime - iridium_datetime).total_seconds()) > self.TIME_ERR_THRESHOLD:
-                os.system(f"""sudo date -s "{iridium_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")}" """) # Update system time
-                os.system("""sudo hwclock -w""") # Write to RTC
-
+                os.system(
+                    f"""sudo date -s "{iridium_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")}" """)  # Update system time
+                os.system("""sudo hwclock -w""")  # Write to RTC
 
     @wrap_errors(LogicalError)
     def systems_check(self) -> list:
@@ -151,5 +148,3 @@ class Mode:
             if self.sfr.devices[device] is not None and not self.sfr.devices[device].functional():
                 result.append(device)
         return result
-
-
