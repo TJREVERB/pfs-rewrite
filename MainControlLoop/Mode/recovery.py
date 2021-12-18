3cc9d9503e23e86496a7c55763311a7574f3531a
Nicolas-M1 <nickmakovnik@gmail.com>
2021-12-18 17:46:00 -0500

    Started recovery implementation

--- MainControlLoop/Mode/recovery.py
+++ MainControlLoop/Mode/recovery.py
@@ -1,9 +1,11 @@
 from MainControlLoop.Mode.mode import Mode
 from lib.exceptions import wrap_errors, LogicalError
 import time
+from MainControlLoop.Drivers.transmission_packet import TransmissionPacket
 
 
 class Recovery(Mode):
+    BEACON_WAIT_TIME = 120  # 2 minutes
 
     @wrap_errors(LogicalError)
     def __init__(self, sfr):
@@ -12,6 +14,7 @@ class Recovery(Mode):
         """
         super().__init__(sfr)
         self.last_contact_attempt = 0
+        self.systems_check_complete = False
 
     @wrap_errors(LogicalError)
     def __str__(self):
@@ -20,22 +23,26 @@ class Recovery(Mode):
     @wrap_errors(LogicalError)  # TODO: IMPLEMENT
     def start(self) -> None:
         super().start([self.sfr.vars.PRIMARY_RADIO])
+        self.sfr.vars.CONTACT_ESTABLISHED = False
 
     @wrap_errors(LogicalError)
     def execute_cycle(self) -> None:  # TODO: IMPLEMENT
-        super().execute_cycle()
         if self.sfr.vars.BATTERY_CAPACITY_INT < self.sfr.vars.LOWER_THRESHOLD:  # Execute cycle low battery
             self.sfr.instruct["All Off"]()  # turn everything off
             time.sleep(self.sfr.vars.ORBITAL_PERIOD)  # sleep for one full orbit
             self.start()
+        else:
+            super().systems_check()  # if systems check runs without throwing an error, everything works
+            if time.time() > self.last_contact_attempt + self.BEACON_WAIT_TIME:  # try to contact ground again
+                self.antenna()  # Antenna deployment, does nothing if antenna is already deployed
+                # Attempt to establish contact with ground
+                print("Transmitting proof of life...")
+                self.sfr.command_executor.GPL(TransmissionPacket("GPL", [], 0))
+                self.last_contact_attempt = time.time()
 
     @wrap_errors(LogicalError)
     def suggested_mode(self) -> Mode:
         super().suggested_mode()
-
-        if not self.sfr.vars.ANTENNA_DEPLOYED:  # just in case this slipped through, go to startup mode
-            return self.sfr.modes_list["Startup"](self.sfr)
-
         science_mode_complete = self.sfr.vars.SIGNAL_STRENGTH_VARIABILITY != -1
         if False:  # TODO: implement conditions for staying in recovery mode
             return self
