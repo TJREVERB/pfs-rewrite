3c7cef3023d7ddd070314ea1abd9dc53d171ed27
Nicolas-M1 <nickmakovnik@gmail.com>
2021-12-18 17:48:16 -0500

    Fixed it...

--- MainControlLoop/Mode/recovery.py
+++ MainControlLoop/Mode/recovery.py
@@ -32,7 +32,9 @@ class Recovery(Mode):
             time.sleep(self.sfr.vars.ORBITAL_PERIOD)  # sleep for one full orbit
             self.start()
         else:
-            super().systems_check()  # if systems check runs without throwing an error, everything works
+            if not self.systems_check_complete:
+                super().systems_check()  # if systems check runs without throwing an error, everything works
+                self.systems_check_complete = True
             if time.time() > self.last_contact_attempt + self.BEACON_WAIT_TIME:  # try to contact ground again
                 self.antenna()  # Antenna deployment, does nothing if antenna is already deployed
                 # Attempt to establish contact with ground
@@ -43,15 +45,14 @@ class Recovery(Mode):
     @wrap_errors(LogicalError)
     def suggested_mode(self) -> Mode:
         super().suggested_mode()
-        science_mode_complete = self.sfr.vars.SIGNAL_STRENGTH_VARIABILITY != -1
-        if False:  # TODO: implement conditions for staying in recovery mode
+        if self.systems_check_complete and self.sfr.vars.CONTACT_ESTABLISHED:  # we are done with recovery mode
             return self
         elif self.sfr.vars.BATTERY_CAPACITY_INT < self.sfr.vars.LOWER_THRESHOLD:  # if we need to enter charging mode
-            if science_mode_complete:  # if we have already finished science, charging will go to outreach
+            if self.sfr.vars.SIGNAL_STRENGTH_VARIABILITY != -1:  # if we have already finished science, charging will go to outreach
                 return self.sfr.modes_list["Charging"](self.sfr, self.sfr.modes_list["Outreach"])
             else:  # we need to charge, then go to science
                 return self.sfr.modes_list["Charging"](self.sfr, self.sfr.modes_list["Science"])
-        elif science_mode_complete:  # If we've finished getting our data
+        elif self.sfr.vars.SIGNAL_STRENGTH_VARIABILITY != -1:  # If we've finished getting our data
             return self.sfr.modes_list["Outreach"](self.sfr)
         else:  # if we still need to finish science mode, and we are done with recovery mode
             return self.sfr.modes_list["Science"](self.sfr)
