427b5709e7f2a808a2373d8f4d482e3a7e1d5438
Nicolas-M1 <nickmakovnik@gmail.com>
2021-12-18 17:59:19 -0500

    Implemented contact attempt acknowledgment command

--- MainControlLoop/lib/command_executor.py
+++ MainControlLoop/lib/command_executor.py
@@ -47,7 +47,8 @@ class CommandExecutor:
             "ITM": self.ITM,
             "IPC": self.IPC,
             "ICE": self.ICE,
-            "IGO": self.IGO
+            "IGO": self.IGO,
+            "IAK": self.IAK
         }
 
         # IMPLEMENT FULLY: Currently based off of Alan's guess of what we need
@@ -580,5 +581,11 @@ class CommandExecutor:
     @wrap_errors(CommandExecutionException)
     def IGO(self, packet: TransmissionPacket):
         self.sfr.vars.ENABLE_SAFE_MODE = False
+        self.transmit(packet, result := [])
+        return result
+
+    @wrap_errors(CommandExecutionException)
+    def IAK(self, packet: TransmissionPacket):
+        self.sfr.vars.CONTACT_ESTABLISHED = True
         self.transmit(packet, result := [])
         return result
