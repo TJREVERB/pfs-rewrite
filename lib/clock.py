41376d2a0c9655f1f9628e6270a95c3550f939b8
Nikhil Kalidasu <skdc13129@gmail.com>
2021-12-20 10:11:42 -0500

    bugfix

--- lib/clock.py
+++ lib/clock.py
@@ -10,7 +10,9 @@ class Clock:
         :param func: function to run
         :param delay: time to wait (seconds)
         """
-        self.func, self.delay, self.last_iteration = func, delay, 0
+        self.func = func
+        self.delay = delay
+        self.last_iteration = 0
 
     @wrap_errors(LogicalError)
     def execute(self) -> bool:
@@ -18,7 +20,8 @@ class Clock:
         Execute function if enough time has passed, otherwise update last iteration
         Conditions are checked IN ORDER!!! If condition 1 fails, condition 2 is not run
         """
-        if t := time.time() > self.last_iteration + self.delay:  # If enough time has passed
-            self.last_iteration = t  # Update last iteration
-            return self.func()  # Run function, return whether it ran
-        return False  # Return False if function was not run
+        if time.time() > self.last_iteration + self.delay:  # If enough time has passed
+            result = self.func()  # Run function, return whether it ran
+            self.last_iteration = time.time()  # Update last iteration
+            return result
+        return False
