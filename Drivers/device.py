9825a040edd081cc611ff4d93d380e2f64b070da
Nikhil Kalidasu <skdc13129@gmail.com>
2021-12-20 09:46:58 -0500

    bugfix

--- Drivers/device.py
+++ Drivers/device.py
@@ -1,8 +1,5 @@
-from lib.registry import StateFieldRegistry
-
-
 class Device:
-    def __init__(self, sfr: StateFieldRegistry):
+    def __init__(self, sfr):
         self.sfr = sfr
 
     # general check of the device, returns True if the device is confirmed to be (somewhat) working
