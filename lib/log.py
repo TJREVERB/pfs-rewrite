0514caf3bc2bfba38b4a82ce921103ae98a7a315
Nikhil Kalidasu <skdc13129@gmail.com>
2021-12-29 13:01:51 -0500

    condensed execute in command executor

--- lib/log.py
+++ lib/log.py
@@ -103,4 +103,4 @@ class Logger:
     @wrap_errors(LogicalError)
     def log(self):
         for i in self.loggers.keys():
-            self.loggers[i].execute()
+            self.loggers[i].execute_buffers()
