ef85301bc20e8d6600891cc132da4187caa058d0
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-01-31 11:52:35 -0500

    stuff

--- MainControlLoop/Mode/outreach/chess_game.py
+++ MainControlLoop/Mode/outreach/chess_game.py
@@ -17,11 +17,11 @@ class ChessGame:
         self.board.set_fen(fen)
 
     def get_best_move(self) -> chess.Move:
-        #engine = chess.engine.SimpleEngine.popen_uci(r'MainControlLoop/Mode/outreach/stockfish_general-32.exe')
-        #result = engine.play(self.board, chess.engine.Limit(self.sfr.vars.OUTREACH_MAX_CALCULATION_TIME))
-        #engine.quit()
-
-        return play(self.board, 5)
+        engine = chess.engine.SimpleEngine.popen_uci(r'MainControlLoop/Mode/outreach/a-stockf')
+        result = engine.play(self.board, chess.engine.Limit(self.sfr.vars.OUTREACH_MAX_CALCULATION_TIME))
+        engine.quit()
+        return result
+        #return play(self.board, 5)
         # TODO: test file path on pi
 
     def push(self, move: chess.Move):  # uci string as move (i.e. d2d4)
