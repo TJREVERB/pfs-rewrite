cda8bb0941eaca2498da0742b5dc14203c9a5a17
Alan-JH <alanjhsu08@gmail.com>
2022-02-04 17:34:30 -0500

    Update mode.py

--- MainControlLoop/Mode/mode.py
+++ MainControlLoop/Mode/mode.py
@@ -109,6 +109,7 @@ class Mode:
         current_datetime = datetime.datetime.utcnow()
         iridium_datetime = self.sfr.devices["Iridium"].processed_time()
         if abs((current_datetime - iridium_datetime).total_seconds()) > self.TIME_ERR_THRESHOLD:
+            print("Updating time")
             os.system(f"sudo date -s \"{iridium_datetime.strftime('%Y-%m-%d %H:%M:%S UTC')}\" ")  
             # Update system time
             os.system("sudo hwclock -w")  # Write to RTC
