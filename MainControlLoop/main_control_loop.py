7b63331b69402a931e83260e51d6fd3143005a8e
The-A44 <arthur.prudius@gmail.com>
2022-02-04 10:07:04 -0500

    rudimentary heartbeat

--- MainControlLoop/main_control_loop.py
+++ MainControlLoop/main_control_loop.py
@@ -1,6 +1,8 @@
 import time
+from Drivers.transmission_packet import UnsolicitedData
 from lib.registry import StateFieldRegistry
 from lib.exceptions import wrap_errors, LogicalError
+from lib.clock import Clock
 from MainControlLoop.Mode.science import Science
 from MainControlLoop.Mode.recovery import Recovery
 from MainControlLoop.Mode.startup import Startup
@@ -11,9 +13,14 @@ class MainControlLoop:
     @wrap_errors(LogicalError)
     def __init__(self, sfr: StateFieldRegistry):
         self.sfr = sfr
+        self.heartbeat = (Clock(600), self.sfr.command_executor.USM) # TODO: Use real heartbeat times
 
     @wrap_errors(LogicalError)
     def start(self):
+        """
+        Logs startup time in sfr and powers on all locked on devices and IMU.
+        Instantiates correct startup mode and starts it.
+        """
         print("MCL Start")
         self.sfr.vars.LAST_STARTUP = time.time()
         self.sfr.power_on("IMU")
@@ -28,6 +35,10 @@ class MainControlLoop:
 
     @wrap_errors(LogicalError)
     def iterate(self):  # Repeat main control loop forever
+        """
+        Iterates mode and checks if the mode should change if there isn't a mode lock.
+        Executes command buffers, logs data, and transmits heartbeat.
+        """
         self.sfr.MODE.execute_cycle()  # Execute single cycle of mode
         if not self.sfr.vars.MODE_LOCK:
             if not isinstance(self.sfr.MODE, type(new_mode := self.sfr.MODE.suggested_mode())):
@@ -35,6 +46,10 @@ class MainControlLoop:
                 print(f"Debug Print: switching modes, {self.sfr.MODE} to {new_mode}")
                 self.sfr.MODE = new_mode
                 self.sfr.MODE.start()
+
+        if self.heartbeat[0].time_elapsed():
+            self.heartbeat[0](UnsolicitedData("Heartbeat Statistics"))
+            self.heartbeat[0].update_time()
 
         self.sfr.command_executor.execute_buffers()  # Execute commands
         self.sfr.logger.log()  # Logs data
