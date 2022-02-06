2bfa98448a3ea583db9fafe091e84924dc9cf605
Alan-JH <alanjhsu08@gmail.com>
2022-02-05 22:50:23 -0500

    idek

--- MainControlLoop/Mode/mode.py
+++ MainControlLoop/Mode/mode.py
@@ -50,7 +50,7 @@ class Mode:
         :rtype: bool
         """
         print(self.sfr.vars.LOCKED_OFF_DEVICES)
-        if any([i in self.sfr.vars.LOCKED_OFF_DEVICES for i in enabled_components]):
+        if sum([(i in self.sfr.vars.LOCKED_OFF_DEVICES) for i in enabled_components]):
             return False
         self.sfr.all_off(exceptions=enabled_components)
         for component in enabled_components:
