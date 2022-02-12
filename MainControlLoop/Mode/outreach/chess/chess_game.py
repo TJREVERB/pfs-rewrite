76e0900a846be379b9b8b4877aa8efee1c070e69
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-02-12 14:44:27 -0500

    test

--- MainControlLoop/Mode/outreach/chess/chess_game.py
+++ MainControlLoop/Mode/outreach/chess/chess_game.py
@@ -41,11 +41,14 @@ class ChessGame:
         :return: best move
         :rtype: :class: 'chess.Move'
         """
-        print(self.board.is_valid())
-        transport, engine = await chess.engine.popen_uci(r'MainControlLoop/Mode/outreach/chess/stockfish_exe')
-        result = await engine.play(self.board, chess.engine.Limit(time=float(self.sfr.vars.OUTREACH_MAX_CALCULATION_TIME)))
-        await engine.quit()
-        return result.move
+        if self.board.is_valid():
+            transport, engine = await chess.engine.popen_uci(r'MainControlLoop/Mode/outreach/chess/stockfish_exe')
+            result = await engine.play(self.board, chess.engine.Limit(time=float(self.sfr.vars.OUTREACH_MAX_CALCULATION_TIME)))
+            await engine.quit()
+            return result.move
+        else:
+            print("INVALID BOARD")
+            raise Exception
 
     def get_best_move(self):
         asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
