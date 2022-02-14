4d1e595004ceac0dd451fbdec258be09315c4062
Nikhil Kalidasu <skdc13129@gmail.com>
2022-02-14 11:46:38 -0500

    cleanup

--- MainControlLoop/main_control_loop.py
+++ MainControlLoop/main_control_loop.py
@@ -28,8 +28,8 @@ class MainControlLoop:
             self.sfr.power_on(device)
         # Set mode to Recovery if (antenna deployed) or (aprs or ad are locked off), Startup otherwise
         # self.sfr.MODE = Recovery(self.sfr) if self.sfr.vars.ANTENNA_DEPLOYED or \
-        #     ("APRS" in self.sfr.vars.LOCKED_OFF_DEVICES or "Antenna Deployer" in
-        #     self.sfr.vars.LOCKED_OFF_DEVICES) else Startup(self.sfr)
+        #     "APRS" in self.sfr.vars.LOCKED_OFF_DEVICES or "Antenna Deployer" in
+        #     self.sfr.vars.LOCKED_OFF_DEVICES else Startup(self.sfr)
         self.sfr.MODE = Science(self.sfr)  # TODO: REMOVE THIS DEBUG LINE
         self.sfr.MODE.start()
 
