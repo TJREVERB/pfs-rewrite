e196b923564d00ef165dd23f61ad3382a6c500a9
Nikhil Kalidasu <skdc13129@gmail.com>
2022-02-15 11:48:46 -0500

    string cast packet.args

--- lib/command_executor.py
+++ lib/command_executor.py
@@ -694,7 +694,7 @@ class CommandExecutor:
         result = []
         string = False
         print(packet.args[0])
-        exec(f"{packet.args[0]}")  # Set result and string inside the exec string if return data is needed
+        exec(str(packet.args[0]))  # Set result and string inside the exec string if return data is needed
         print(result, string)
         self.transmit(packet, result, string)
         return result
