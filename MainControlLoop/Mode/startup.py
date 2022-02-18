31ef583f8d74df08fb905a6a404006b61c78f70e
Alan-JH <alanjhsu08@gmail.com>
2022-02-18 18:17:13 -0500

    Update startup.py

--- MainControlLoop/Mode/startup.py
+++ MainControlLoop/Mode/startup.py
@@ -103,7 +103,8 @@ class Startup(Mode):
         super().execute_cycle()
         if self.sfr.check_lower_threshold():  # Execute cycle low battery
             self.sfr.all_off()  # turn everything off
-            self.sfr.sleep(5400)  # sleep for one full orbit
+            print("Sleeping")
+            self.sfr.sleep(120)  # sleep for one full orbit #TODO: replace with 5400
             self.start()  # Run start again to turn on devices
         # Make sure primary radio is on (may change in mission control if Iridium packets don't transmit)
         self.sfr.power_on(self.sfr.vars.PRIMARY_RADIO)
