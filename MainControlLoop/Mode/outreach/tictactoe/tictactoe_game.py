bb4d804bc004b9c665ca6e774aad945d3cf8c0e8
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-02-18 22:10:47 -0500

    bugfix

--- MainControlLoop/Mode/outreach/tictactoe/tictactoe_game.py
+++ MainControlLoop/Mode/outreach/tictactoe/tictactoe_game.py
@@ -64,13 +64,11 @@ class TicTacToeGame:
 
     def get_best_move(self):
         path = r"MainControlLoop/Mode/outreach/tictactoe/table.json"
-        f = open(path, "r")
-        table = json.load(f)
-        f.close()
+        with open(path, "r") as f:
+            table = json.load(f)
         game_string = str(self).split(';')[1]
         if game_string in table:
             return list(table[game_string])
-        del table
 
         e  # to crash script
         # WILL NEVER EXECUTE
