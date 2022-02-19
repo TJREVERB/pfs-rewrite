3609651bf61418568c808d43e9598d9a2626e81d
Nikhil Kalidasu <skdc13129@gmail.com>
2022-02-18 19:29:42 -0500

    documentation

--- MainControlLoop/Mode/outreach/ultimate_tictactoe/tictactoe3x3.py
+++ MainControlLoop/Mode/outreach/ultimate_tictactoe/tictactoe3x3.py
@@ -3,18 +3,23 @@ import copy
 import random
 
 
-def binary_scale(n):
-    for i in range(n, 0, -1):
-        yield 2 ** n
-
-
 class TicTacToe:
-    def __init__(self, is_ai_turn):
-        self.is_ai_turn = is_ai_turn
+    """
+    TicTacToe game (used in Ultimate TicTacToe and MCTS)
+    """
+    def __init__(self, is_ai_turn: bool):
+        """
+        Create a new game
+        :param is_ai_turn: whether we're starting this game as AI or human
+        :type is_ai_turn: bool
+        """
+        self.is_ai_turn = is_ai_turn  # Whether this is currently the ai turn (used for testing)
+        # Initialize boards as 0
         self.human_board = 0
         self.ai_board = 0
+        # Preprocessed winning combinations
         self.winning_combinations = [0x1C0, 0x38, 0x7, 0x124, 0x92, 0x49, 0x111, 0x54]
-        self.draw_combination = 0x1FF
+        self.draw_combination = 0x1FF  # Only possible draw combination in tictactoe
 
     def __str__(self):  # returns encoded board
         """
@@ -26,7 +31,7 @@ class TicTacToe:
         x-ooox--x
         """
         encoded_string = ""
-        for i in binary_scale(9):
+        for i in range(9):  # Iterate over bits
             if self.human_board >> i & 1:
                 encoded_string += "x"
             elif self.ai_board >> i & 1:
@@ -35,28 +40,42 @@ class TicTacToe:
                 encoded_string += "-"
         return encoded_string
 
-    def board_string(self, row):  # for testing, human only
+    def board_string(self, row: int):  # for testing, human only
+        """
+        Returns printable version of board row
+        :param row: row to convert to string
+        :type row: int
+        """
         row = row % 3
         string = ""
         for col in range(3):
             if self.human_board >> row * 3 + col:  # if human piece
                 string += 'x'
-            elif self.ai_board >> row * 3 + col:
+            elif self.ai_board >> row * 3 + col:  # if ai piece
                 string += 'o'
-            else:
+            else:  # if no piece
                 string += '.'
             string += " | "
         return string.strip()
 
     def set_game(self, board_string: str):
-        """Sets board to proper board according to string"""
-        for i in binary_scale(9):
+        """
+        Loads board from string
+        :param board_string: string to load from
+        :type board_string: str
+        """
+        for i in range(9):
             if board_string[i] == "x":
-                self.human_board += i
+                self.human_board += 2 ** i
             elif board_string[i] == "o":
-                self.ai_board += i
+                self.ai_board += 2 ** i
 
-    def check_winner(self) -> int:  # (x_status, o_status) 0 = human win, 1 = draw, 2 = ai win, -1 = game incomplete
+    def check_winner(self) -> int:
+        """
+        Checks the winner of the current board
+        :return: 0 = human win, 1 = draw, 2 = ai win, -1 = game incomplete
+        :rtype: int
+        """
         if self.human_board in self.winning_combinations:
             return 0
         elif self.ai_board in self.winning_combinations:
@@ -68,18 +87,35 @@ class TicTacToe:
         return -1
 
     def switch_turn(self):
+        """
+        Switches current player (ai to human and vice versa)
+        """
         self.is_ai_turn = not self.is_ai_turn
 
-    def is_valid_move(self, location: int) -> int:
-        return ~(self.human_board & self.ai_board) >> location & 1
+    def is_valid_move(self, location: int) -> bool:
+        """
+        Check if a given move is valid
+        :param location: move to test
+        :type location: int
+        :return: whether this move is valid
+        :rtype: bool (0 or 1)
+        """
+        return bool(~(self.human_board & self.ai_board) >> location & 1)
 
     def get_valid_moves(self) -> [int]:
         """
-        returns all valid moves as list of integer moves
+        Get all valid moves on this board
+        :return: list of integer moves
+        :rtype: list
         """
-        return [i for i in binary_scale(9) if self.is_valid_move(i)]
+        return [i for i in range(9) if self.is_valid_move(i)]
 
     def push(self, move: int) -> None:  # Takes integer move
+        """
+        Update board state with a given move
+        :param move: move to update with
+        :type move: int
+        """
         if self.is_ai_turn:
             self.ai_board += move
         else:
@@ -87,12 +123,22 @@ class TicTacToe:
         self.switch_turn()
 
     def push_move_to_copy(self, move: int):
-        """returns new game object with pushed move"""
+        """
+        Copies game object, applies move, and returns
+        :param move: move to make
+        :type move: int
+        :return: new game object
+        :rtype: TicTacToe
+        """
         new_board = copy.deepcopy(self)
         new_board.push(move)
         return new_board
 
-    def random(self):
+    @staticmethod
+    def random() -> str:
+        """
+        Generate a random board (TESTING ONLY)
+        """
         board = TicTacToe(True)
         for _ in range(4):
             board.push(random.choice(board.get_valid_moves()))
