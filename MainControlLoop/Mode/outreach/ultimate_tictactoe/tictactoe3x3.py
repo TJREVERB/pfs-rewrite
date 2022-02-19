b3c1b2e77ead9ee56e0b53011663f8f5faaf7e53
Nikhil Kalidasu <skdc13129@gmail.com>
2022-02-18 19:14:57 -0500

    optimize tictactoe

--- MainControlLoop/Mode/outreach/ultimate_tictactoe/tictactoe3x3.py
+++ MainControlLoop/Mode/outreach/ultimate_tictactoe/tictactoe3x3.py
@@ -3,11 +3,16 @@ import copy
 import random
 
 
+def binary_scale(n):
+    for i in range(n, 0, -1):
+        yield 2 ** n
+
+
 class TicTacToe:
     def __init__(self, is_ai_turn):
         self.is_ai_turn = is_ai_turn
-        self.human_board = np.zeros((3, 3))  # X
-        self.ai_board = np.zeros((3, 3))  # O
+        self.human_board = 0
+        self.ai_board = 0
         self.winning_combinations = [0x1C0, 0x38, 0x7, 0x124, 0x92, 0x49, 0x111, 0x54]
         self.draw_combination = 0x1FF
 
@@ -21,121 +26,76 @@ class TicTacToe:
         x-ooox--x
         """
         encoded_string = ""
-        human_board = self.human_board.reshape((9,))
-        ai_board = self.ai_board.reshape((9,))
-        for i in range(9):
-            if int(human_board[i]) == 1:
+        for i in binary_scale(9):
+            if self.human_board >> i & 1:
                 encoded_string += "x"
-            elif int(ai_board[i]) == 1:
+            elif self.ai_board >> i & 1:
                 encoded_string += "o"
             else:
                 encoded_string += "-"
-        return f"{encoded_string}"
+        return encoded_string
 
     def board_string(self, row):  # for testing, human only
         row = row % 3
         string = ""
-        human_list = np.reshape(self.human_board, (9,))
-        ai_list = -1 * np.reshape(self.ai_board, (9,))
-
-        combined_list = list(map(int, (human_list + ai_list).tolist()))
         for col in range(3):
-            string += " "
-            if combined_list[row*3 + col] == 1:  # if human piece
+            if self.human_board >> row * 3 + col:  # if human piece
                 string += 'x'
-            elif combined_list[row*3 + col] == -1:
+            elif self.ai_board >> row * 3 + col:
                 string += 'o'
             else:
                 string += '.'
-            string += " |"
-        return string
+            string += " | "
+        return string.strip()
 
     def set_game(self, board_string: str):
         """Sets board to proper board according to string"""
-        human_board = np.zeros((9,))
-        ai_board = np.zeros((9,))
-        for i in range(9):
+        for i in binary_scale(9):
             if board_string[i] == "x":
-                human_board[i] = 1.0
+                self.human_board += i
             elif board_string[i] == "o":
-                ai_board[i] = 1.0
-        self.human_board = human_board.reshape((3, 3))
-        self.ai_board = ai_board.reshape((3, 3))
-        return self
+                self.ai_board += i
 
-    def check_winner(self) -> tuple:  # (x_status, o_status) 0 = no winner, 1 = won, (1, 1) if draw
-        human_bitboard = self.get_bitboard(self.human_board)
-        ai_bitboard = self.get_bitboard(self.ai_board)
-        for binary in self.winning_combinations:
-            if (binary & human_bitboard) in self.winning_combinations:
-                return 1, 0
-            elif (binary & ai_bitboard) in self.winning_combinations:
-                return 0, 1
-        if human_bitboard | ai_bitboard == self.draw_combination:
-            return 1, 1
-        return 0, 0
+    def check_winner(self) -> int:  # (x_status, o_status) 0 = human win, 1 = draw, 2 = ai win, -1 = game incomplete
+        if self.human_board in self.winning_combinations:
+            return 0
+        elif self.ai_board in self.winning_combinations:
+            return 2
+        elif self.human_board == self.draw_combination:  # Split to two if statements for efficiency
+            return 1
+        elif self.ai_board == self.draw_combination:
+            return 1
+        return -1
 
     def switch_turn(self):
-        if self.is_ai_turn:
-            self.is_ai_turn = False
-        else:
-            self.is_ai_turn = True
+        self.is_ai_turn = not self.is_ai_turn
 
-    def is_valid_move(self, location: list) -> bool:
-        x, y = location[0], location[1]
-        if type(x) != int or type(y) != int:
-            return False
-        if x > 2 or x < 0 or y > 2 or y < 0:
-            return False
-        if self.human_board[x][y] == 0 and self.ai_board[x][y] == 0:
-            return True
-        else:
-            return False
+    def is_valid_move(self, location: int) -> int:
+        return ~(self.human_board & self.ai_board) >> location & 1
 
-    def get_valid_moves(self) -> list:
+    def get_valid_moves(self) -> [int]:
         """
-        returns all valid moves as list of lists
+        returns all valid moves as list of integer moves
         """
+        return [i for i in binary_scale(9) if self.is_valid_move(i)]
 
-        possible_moves = [[x, y] for y in range(3) for x in range(3)]
-        legal_move_distribution = list(map(self.is_valid_move, possible_moves))
-        valid_moves = [possible_moves[i] for i in range(9) if legal_move_distribution[i] is True]
-        return valid_moves
-
-    def push(self, location: list) -> None:  # Takes coords as [row, column] i.e. [x, y]
-        location = list(map(int, location))
-        x, y = location[0], location[1]
+    def push(self, move: int) -> None:  # Takes integer move
         if self.is_ai_turn:
-            self.ai_board[x][y] = 1.0
+            self.ai_board += move
         else:
-            self.human_board[x][y] = 1.0
+            self.human_board += move
         self.switch_turn()
 
-    def push_move_to_copy(self, location: list):
+    def push_move_to_copy(self, move: int):
         """returns new game object with pushed move"""
         new_board = copy.deepcopy(self)
-        new_board.push(location)
+        new_board.push(move)
         return new_board
 
-    def get_bitboard(self, array: np.array) -> int:
-        new_list = list(np.reshape(array, (9,)))
-        new_list = map(int, new_list)
-        new_list = list(map(str, new_list))
-        bitboard = int("".join(new_list), 2)
-        return bitboard
-
-    def get_board_array(self) -> np.array:  # human piece = 1, ai = -1
-        return np.add(self.human_board, self.ai_board * -1)
-
     def random(self):
-        while True:
-            board = TicTacToe(True)
-            for r in range(3):
-                for c in range(3):
-                    rand = random.randint(0, 2)
-                    if rand == 0:
-                        board.human_board[r, c] = 1.0
-                    else:
-                        board.ai_board[r, c] = 1.0
-            if board.check_winner() == (1, 1):
-                return str(board)
+        board = TicTacToe(True)
+        for _ in range(4):
+            board.push(random.choice(board.get_valid_moves()))
+            if random.random() < 1 / 4:
+                break
+        return str(board)
