1b7a9ea80a1a2b5791f162a7663431ee02659115
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-02-19 15:36:58 -0500

    test

--- MainControlLoop/Mode/outreach/tictactoe/tictactoe_game.py
+++ MainControlLoop/Mode/outreach/tictactoe/tictactoe_game.py
@@ -65,7 +65,7 @@ class TicTacToeGame:
 
     def get_best_move(self):
         file = open("MainControlLoop/Mode/outreach/tictactoe/table.pkl")
-        table = pickle.load(file)
+        table = json.load(file)
         if not file.closed:
             file.close()
         #with open("MainControlLoop/Mode/outreach/tictactoe/table.pkl", "rb") as f:
