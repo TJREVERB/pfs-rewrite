ca14697554eab776f571a8a8b297b8bfd357e268
Nikhil Kalidasu <skdc13129@gmail.com>
2022-02-23 19:28:05 -0500

    undo remove

--- MainControlLoop/Mode/startup.py
+++ MainControlLoop/Mode/startup.py
@@ -67,10 +67,10 @@ class Startup(Mode):
             print("Time not elapsed")
             return False
         # If we haven't yet reached the maximum threshold of time to wait for antenna deployment
-        # if not time.time() > self.sfr.vars.START_TIME + self.ANTENNA_MAXIMUM_THRESHOLD:
-        #     if self.sfr.devices["IMU"].is_tumbling():  # If we're still tumbling
-        #         print("currently tumbling")
-        #         return False  # Do nothing
+        if not time.time() > self.sfr.vars.START_TIME + self.ANTENNA_MAXIMUM_THRESHOLD:
+            if self.sfr.devices["IMU"].is_tumbling():  # If we're still tumbling
+                print("currently tumbling")
+                return False  # Do nothing
         # Enable power to antenna deployer
         self.sfr.power_on("Antenna Deployer")
         time.sleep(5)
