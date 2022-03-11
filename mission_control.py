bac8d027126f6d871be704bddc672ce134c560a7
Nicolas-M1 <nickmakovnik@gmail.com>
2022-03-11 15:56:12 -0500

    yes

--- mission_control.py
+++ mission_control.py
@@ -74,8 +74,8 @@ class MissionControl:
                 except Exception as e:  # If a problem happens
                     print("Caught exception (printed from mission_control line 75)")
                     if not self.troubleshoot(e):  # If built-in troubleshooting fails
-                        # self.testing_mode(e)  # Debug
-                        self.error_handle(e)  # Handle error, uncomment when done testing low level things
+                        self.testing_mode(e)  # TODO: Debug
+                        #self.error_handle(e)  # Handle error, uncomment when done testing low level things
                     # Move on with MCL if troubleshooting solved problem (no additional exception)
             # If any packet has been in the queue for too long and APRS is not locked off, switch primary radio
             if any([i.get_packet_age() > self.sfr.vars.PACKET_AGE_LIMIT for i in self.sfr.vars.transmit_buffer]):
