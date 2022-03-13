523b277e4c9fc6ddb421403b7650b2868e7ac19d
Khoi Dinh <65259797+TheFatKoala@users.noreply.github.com>
2022-03-13 16:06:06 -0400

    .

--- MainControlLoop/Mode/startup.py
+++ MainControlLoop/Mode/startup.py
@@ -81,8 +81,9 @@ class Startup(Mode):
                 print("currently tumbling")
                 return False  # Do nothing
         # Enable power to antenna deployer
-        self.sfr.power_on("Antenna Deployer")
         time.sleep(5)
+        self.sfr.power_on("Antenna Deployer")
+        time.sleep(10)
         self.sfr.devices["Antenna Deployer"].deploy()  # Deploy antenna
         print("Antenna deployment attempted")
         self.sfr.sleep(30)
