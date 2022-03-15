8117badb9d7cf9ee2279e5e10d692173b56bbf18
tjhsstreverb <tjreverb@gmail.com>
2022-03-15 02:53:32 +0000

    update readoutput

--- readoutput.py
+++ readoutput.py
@@ -4,7 +4,8 @@ lastlen = 0
 while 1:
 	f = open("/home/pi/pfs-rewrite/pfs-output.txt", "r")
 	ls = f.readlines()
-	print("".join(ls[lastlen:]))
+	if len(ls) > lastlen:
+		print("".join(ls[lastlen:]))
 	lastlen = len(ls)
 	f.close()
 	time.sleep(.5)
