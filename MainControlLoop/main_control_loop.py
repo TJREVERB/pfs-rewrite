07ffdf09adc430b3260e8b1b7533fdfdbff834c4
Nikhil Kalidasu <skdc13129@gmail.com>
2022-03-15 10:51:26 -0400

    fixed print

--- MainControlLoop/main_control_loop.py
+++ MainControlLoop/main_control_loop.py
@@ -19,7 +19,7 @@ class MainControlLoop:
         Logs startup time in sfr and powers on all locked on devices and IMU.
         Instantiates correct startup mode and starts it.
         """
-        print("MCL Start", file = open("pfs-output.txt", "a"))
+        print("MCL Start", file=open("pfs-output.txt", "a"))
         self.sfr.vars.LAST_STARTUP = time.time()
         self.sfr.power_on("IMU")
         for device in self.sfr.vars.LOCKED_ON_DEVICES:  # power on all devices that are locked on
@@ -37,17 +37,19 @@ class MainControlLoop:
         Executes command buffers and logs data.
         """
         self.sfr.MODE.execute_cycle()  # Execute single cycle of mode
-        print(f"Transmit buffer looks like this: {self.sfr.vars.transmit_buffer}", file = open("pfs-output.txt", "a"))  # TODO: DELETE THIS AFTER TESTING ICT
+        print(f"Transmit buffer looks like this: {[str(i) for i in self.sfr.vars.transmit_buffer]}",
+              file=open("pfs-output.txt", "a"))  # TODO: DELETE THIS AFTER TESTING ICT
         # Change modes while there isn't a mode lock or there is low battery
         print(f"Commands {[p.descriptor for p in self.sfr.vars.command_buffer]}")
         self.sfr.command_executor.execute_buffers()  # Execute commands
 
         if not self.sfr.vars.MODE_LOCK or self.sfr.check_lower_threshold():
             if not isinstance(self.sfr.MODE, type(new_mode := self.sfr.MODE.suggested_mode())):
-                print(f"Debug Print: switching modes, {self.sfr.MODE} to {new_mode}", file = open("pfs-output.txt", "a"))
+                print(f"Debug Print: switching modes, {self.sfr.MODE} to {new_mode}", file=open("pfs-output.txt", "a"))
                 self.sfr.switch_mode(new_mode)
                 if not self.sfr.switch_mode(new_mode):
                     self.sfr.MODE.start()  # restarts the current mode to turn devices back on
-                    print(f"Switch failed because of locked components! Staying in {self.sfr.MODE}", file = open("pfs-output.txt", "a"))
+                    print(f"Switch failed because of locked components! Staying in {self.sfr.MODE}",
+                          file=open("pfs-output.txt", "a"))
 
         self.sfr.logger.log()  # Logs data
